9ef5d4ed3505283c560460c239ecbbfea69aa23d
Dejan21 <jangelovski_dejan@yahoo.com>
2022-05-19 22:04:45 +0200

    POST request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,26 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage(props)  {
+
+    function addMeetupHandler(meetupData){
+        fetch(
+            'https://react-getting-started-94af0-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+             {
+                 method: 'POST',
+                 body: JSON.stringify(meetupData),
+                 headers: {
+                     'Content-Type' : 'application/json'
+                 }
+             }
+         );
+
+
+    }
+
     return (
         <section >
            <h1>Add New Meetup</h1>
-           <NewMeetupForm />
+           <NewMeetupForm  onAddMeetup={addMeetupHandler}/>
         </section>
     );
 };
